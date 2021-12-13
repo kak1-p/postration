5d92e7afaaa96b48870a198d5118b7399a147eb2
kak1-p <kak1.ppp456@gmail.com>
2021-12-13 18:27:05 +0900

    マイページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @posts = Post.all
@@ -18,16 +20,13 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to root_path
     else
@@ -36,11 +35,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   private
   def post_params
     params.require(:post).permit(:image, :detail).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
